c698d1ea1e5516ac5237aeca1d42e74b9bc176d4
Yusufcan Ulusoy <yusufcan.ulusoy@agu.edu.tr>
2024-02-21 15:00:23 +0300

    Auth without token added

--- JWTauth/Program.cs
+++ JWTauth/Program.cs
@@ -2,8 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JWTauth.Interfaces;
+using JWTauth.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -18,6 +22,24 @@ namespace JWTauth
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureServices(services =>
+                    {
+                        services.AddControllers();
+                        services.AddTransient<IAuthService, AuthService>();
+                    }).Configure(app =>
+                    {
+                        var env = app.ApplicationServices.GetService<IWebHostEnvironment>();
+                        if (env.IsDevelopment())
+                        {
+                            app.UseDeveloperExceptionPage();
+                        }
+
+                        app.UseRouting();
+                        app.UseAuthorization();
+                        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+                    });
+                });
     }
 }
---
+++ JWTauth/Services/AuthService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using JWTauth.Interfaces;
+using JWTauth.Models;
+
+namespace JWTauth.Services
+{
+    public class AuthService :IAuthService
+    {
+        public Task<UserLoginResponse> LoginUserAsync(UserLoginRequest request)
+        {
+            UserLoginResponse response = new UserLoginResponse();
+
+            if (string.IsNullOrEmpty(request.Username) || string.IsNullOrEmpty(request.Password))
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            if (request.Username == "yusuf" && request.Password == "123456")
+            {
+                response.AccessTokenExpireDate = DateTime.UtcNow;
+                response.AuthenticateResult = true;
+                response.AuthToken = string.Empty;
+            }
+
+            return Task.FromResult(response);
+        }
+
+    }
+}
